f0ae79de8893c234581e7b4f8320d70e91f10d31
Gonzalo Silva <30927470+gonzalosil@users.noreply.github.com>
2024-07-03 19:37:16 -0300

    jygf

---
+++ menu.py
@@ -0,0 +1,7 @@
+from paquete1.modulo1 import Cliente
+
+cliente1 = Cliente("Gonzalo", "Silva", "39", "39489109", "1156293183")
+print(cliente1)
+cliente1.comprar("Laptop", "mercado libre")
+cliente1.cambiar_telefono("1234567890")
+print(cliente1)
